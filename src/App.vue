ef37d25a71581f5bd8843e4077e64469dffcf904
yourname <youremail@address.com>
2020-06-10 16:47:30 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,555 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app class="display">
+    <v-app-bar
+      class="borderd"
+      color="white"
+      app
+      light
+      elevate-on-scroll
+      flat
+    >
+        <v-toolbar-title style="padding:0px" class="title font-weight-bold">หน้าแรก</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        <v-btn text fab>
+        <v-icon color="#42A5F5"> mdi-star-four-points-outline</v-icon>
+        </v-btn>
+    </v-app-bar>
+
+    <v-divider></v-divider>
+
+    <!-- <v-layout >
+      <v-flex > -->
+
+    <v-navigation-drawer
+      permanent
+      light
+      app
+      width="320"
+      v-model="drawer"
+      :mini-variant="isMobile"
+      mini-variant-width = "80"
+    >
+
+    <v-list-tile>
+
+      <v-list v-if="!isMobile" :class="isMobile && 'px-0'">
+      <div class="ml-10">
+
+        <v-btn fab light text color="#42A5F5" >
+       <v-icon medium>mdi-twitter</v-icon>
+       </v-btn>
+
+      </div>
+      </v-list>
+
+      <v-list v-else :class="isMobile && 'px-0'">
+      <div class="ml-3">
+
+        <v-btn fab light text color="#42A5F5" >
+       <v-icon medium>mdi-twitter</v-icon>
+       </v-btn>
+
+      </div>
+      </v-list>
+
+    </v-list-tile>
+
+    <v-item-group v-if="!isMobile"  mandatory class="ml-10 mr-5">
+
+    <v-list v-for="list in lists" :key="list.text">
+
+      <v-item v-slot:default="{ active, toggle }">
+      <!-- <v-col cols="2"> -->
+      <v-btn
+      rounded
+      text
+      light
+      class="title font-weight-bold hove"
+      :color="active ? '#42A5F5' : ''"
+      @click="toggle"
+      >
+
+            <v-list-icon>
+              <v-icon medium>{{ list.icon }}</v-icon>
+            </v-list-icon>
+
+            <v-list-content class="ml-5 mr-5">
+              <v-list-title >{{ list.text }}</v-list-title>
+            </v-list-content>
+
+      </v-btn>
+      <!-- </v-col> -->
+</v-item>
+    </v-list>
+
+    </v-item-group>
+
+    <v-item-group v-else mandatory class="ml-5">
+
+    <v-list v-for="list in lists" :key="list.text">
+
+      <v-item v-slot:default="{ active, toggle }">
+      <!-- <v-col cols="2"> -->
+      <v-btn
+      icon
+      rounded
+      text
+      light
+      class="title font-weight-bold hove"
+      :color="active ? '#42A5F5' : ''"
+
+      @click="toggle"
+      >
+
+            <v-list-icon>
+              <v-icon medium>{{ list.icon }}</v-icon>
+            </v-list-icon>
+
+      </v-btn>
+      <!-- </v-col> -->
+</v-item>
+    </v-list>
+
+    </v-item-group>
+
+    <v-list  v-if="!isMobile" class="ml-10 mr-5">
+
+    <v-btn
+      rounded
+      color="#42A5F5"
+      dark
+      class="font-weight-bold mr-10"
+      block
+      >
+      ทวีต
+      </v-btn>
+    </v-list>
+
+    <v-list  v-else class="ml-3 ">
+     <v-btn
+      fab
+      rounded
+      light
+      color="#42A5F5"
+
+      class="title font-weight-bold"
+      @click="toggle"
+      >
+              <v-icon dark color="white" >mdi-pencil-plus-outline</v-icon>
+
+      </v-btn>
+    </v-list>
+
+    <v-list v-if="!isMobile" class="ml-10 mr-5">
+    <!-- <v-btn  @click="show = !show">USERNAME</v-btn> -->
+
+    <v-menu
+              bottom
+              top
+              offset-y
+            >
+              <template v-slot:activator="{ on }">
+              <!-- New Import -->
+
+  <v-card
+    flat
+    max-width="500"
+    class="mx-0 mt-2"
+    style="border-radius: 50px"
+  >
+
+  <v-list two-line>
+      <template v-for="item in item2" >
+
+        <v-list-item
+          :key="item.title"
+          @click="doSomething()"
+          v-on="on"
+        >
+          <v-list-item-avatar>
+            <v-img :src="item.avatar"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title class="font-weight-bold" v-html="item.title"></v-list-item-title>
+            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+          <v-spacer></v-spacer>
+          </v-list-item-content>
+
+          <v-icon rounded class="font-weight-bold" light >mdi-chevron-down</v-icon>
+        </v-list-item>
+
+      </template>
+    </v-list>
+
+  </v-card>
+              <!-- New Import -->
+              </template>
+
+      <v-list>
+
+      <template v-for="item in item2" >
+        <v-list-item
+          :key="item.title"
+        >
+          <v-list-item-avatar>
+            <v-img :src="item.avatar"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title class="font-weight-bold" v-html="item.title"></v-list-item-title>
+            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+          <v-spacer></v-spacer>
+
+
+          </v-list-item-content>
+          <v-icon color="#42A5F5">mdi-check</v-icon>
+          <!-- <v-btn rounded text outlined class="font-weight-bold" color="primary" style="border: 1px solid currentColor;"> ติดตาม</v-btn> -->
+        </v-list-item>
+
+      </template>
+                <v-list-item
+                  v-for="(item, i) in Dialog"
+                  :key="i"
+                  @click="() => {}"
+                >
+
+                  <v-list-item-title
+                  v-for="(UserAccount,j) in item2"
+                  :key="j"
+                  >
+                    {{ item.title }}
+                    <v-text v-if="item.check">
+                      {{UserAccount.subtitle}}
+                    </v-text>
+
+                    </v-list-item-title>
+
+                </v-list-item>
+              </v-list>
+            </v-menu>
+
+    </v-list>
+    </v-navigation-drawer>
+
+<!-- start Right -->
+
+<v-navigation-drawer
+
+      class="scroll"
+      light
+      app
+      right
+      width="430"
+    >
+
+<v-app-bar flat light shrink-on-scroll color="white">
+     <v-text-field
+            class="mx-1 mt-2 mr-6"
+            solo
+            label="ค้นหาทวิตเตอร์"
+            light
+            rounded
+            flat
+            dense
+            background-color="#f5f8fa"
+            prepend-inner-icon="mdi-magnify"
+      >
+      </v-text-field>
+</v-app-bar>
+
+<v-list>
+<v-card
+    max-width="500"
+    class="mx-5 mt-2 mr-10"
+    style="border-radius: 20px"
+    color="#f5f8fa"
+    flat
+  >
+    <v-toolbar
+      light
+      flat
+      color="#f5f8fa"
+    >
+
+      <v-toolbar-title class="title font-weight-bold">ความนิยมสำหรับคุณ</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon color="#42A5F5">mdi-cog-outline</v-icon>
+      </v-btn>
+
+    </v-toolbar>
+    <v-divider></v-divider>
+  <v-list three-line>
+      <template v-for="right in rightnav" >
+
+        <v-divider
+          v-if="right.divider"
+          :key="right.text"
+        ></v-divider>
+
+        <v-list-item
+          v-else
+          :key="right.tag"
+          @click="doSomething()"
+        >
+
+          <v-list-item-content>
+            <!-- <v-list-item-subtitle v-html="right.tag"></v-list-item-subtitle>
+            <v-list-item-title class="font-weight-bold" v-html="right.text"></v-list-item-title>
+            <v-list-item-subtitle v-html="right.Tweet"></v-list-item-subtitle> -->
+            <v-list-item-subtitle class="caption font-weight-thin">{{right.tag}}</v-list-item-subtitle>
+            <v-list-item-title class="font-weight-bold">{{right.text}}</v-list-item-title>
+            <v-list-item-subtitle>{{right.Tweet}}</v-list-item-subtitle>
+
+          <v-spacer></v-spacer>
+
+          </v-list-item-content>
+ <!-- <v-icon rounded class="font-weight-bold" light >mdi-chevron-down</v-icon> -->
+        <!-- Dropdown ความนิยมสำหรับคุณ -->
+  <v-menu
+      bottom
+      left
+  >
+  <template v-slot:activator="{ on }">
+
+  <v-btn fab light small text top right class="hove">
+        <!-- <v-icon small> mdi-chevron-down</v-icon> -->
+
+      <template v-for="item in item2">
+        <v-list-item
+          :key="item.title"
+          @click="doSomething()"
+          v-on="on"
+        >
+          <v-icon rounded light class="title font-weight-thin">mdi-chevron-down</v-icon>
+        </v-list-item>
+
+      </template>
+
+
+  </v-btn>
+              <!-- New Import -->
+      </template>
+
+      <v-list>
+                <v-list-item
+                  v-for="(item, i) in Report"
+                  :key="i"
+                  @click="() => {}"
+                >
+                <v-list-icon>
+                  <v-icon>
+                  {{item.icon}}
+                  </v-icon>
+                </v-list-icon>
+
+                <v-list-item-title class="body-2 ml-2">
+                  {{ item.reports }}
+                  </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+
+        <!-- Dropdown ความนิยมสำหรับคุณ -->
+
+        </v-list-item>
+
+      </template>
+    </v-list>
+
+    <v-btn
+      text
+      rounded
+      light
+      color="#42A5F5"
+      class="font-weight-bold"
+      >
+      <v-text>
+        แสดงเพิ่มเติม
+      </v-text>
+      </v-btn>
+
+  </v-card>
+</v-list>
+
+<!-- Accout -->
+
+<v-list>
+<v-card
+    max-width="500"
+    class="mx-5 mt-2 mr-10"
+    style="border-radius: 20px"
+    color="#f5f8fa"
+    flat
+  >
+    <v-toolbar
+      light
+      flat
+      color="#f5f8fa"
+    >
+
+      <v-toolbar-title class="title font-weight-bold">ติดตามใครดี</v-toolbar-title>
+
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn> -->
+
+    </v-toolbar>
+    <v-divider></v-divider>
+
+  <v-list two-line>
+      <template v-for="(item, index) in items" >
+
+        <v-divider
+          v-if="item.divider"
+          :key="index"
+        ></v-divider>
+
+        <v-list-item
+          v-else
+          :key="item.title"
+          @click="doSomething()"
+        >
+          <v-list-item-avatar>
+            <v-img :src="item.avatar"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title class="font-weight-bold" v-html="item.title"></v-list-item-title>
+            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+          <v-spacer></v-spacer>
+          </v-list-item-content>
+
+          <v-btn rounded text outlined class="font-weight-bold" color="#42A5F5" style="border: 1px solid currentColor;"> ติดตาม</v-btn>
+        </v-list-item>
+
+      </template>
+    </v-list>
+
+    <v-btn
+      text
+      rounded
+      light
+      color="#42A5F5"
+      class="font-weight-bold"
+      >
+        แสดงเพิ่มเติม
+      </v-btn>
+
+  </v-card>
+</v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+
+  data:() => ({
+    show: false,
+    drawer:true,
+    lists: [
+      {icon: "mdi-home",text: "หน้าแรก"},
+      {icon: "mdi-pound",text: "สำรวจ"},
+      {icon: "mdi-bell-outline",text: "แจ้งเตือน"},
+      {icon: "mdi-email-outline",text: "ข้อความ"},
+      {icon: "mdi-bookmark-outline",text: "บุ๊กมาร์ก"},
+      {icon: "mdi-text-box-outline",text: "รายชื่อ"},
+      {icon: "mdi-account-outline",text: "ข้อมูลส่วนตัว"},
+      {icon: "mdi-dots-horizontal-circle-outline",text: "เพิ่มเติม"}
+    ],
+    miniVariant: false,
+    rightnav: [
+      {tag:"ได้รับความนิยมในไทย",text:"#ยกเลิก112",Tweet:"100K ทวีต"},
+      { divider: true},
+      {tag:"ได้รับความนิยมในไทย",text:"#WhoAreYouEP12",Tweet:"50K ทวีต"},
+      { divider: true},
+      {tag:"ได้รับความนิยมในไทย",text:"#โบ๊ะบ๊ะแฟมิลี่",Tweet:"30K ทวีต"},
+      { divider: true},
+    ],
+   items: [
+        {
+          avatar: require('./assets/Lisa.jpg'),
+          title: 'BLACKPINK LISA 리사',
+          subtitle: "@lalaIalisaa_m",
+        },
+        { divider: true},
+        {
+          avatar: require('./assets/HRK.jpg'),
+          title: 'พี่เป็นนักยูทูป',
+          subtitle: "@MisterHeartz",
+        },
+        { divider: true},
+        {
+          avatar: require('./assets/Artgerm.jpg'),
+          title: 'Stanley Artgerm Lau',
+          subtitle: "@Artgerm",
+        },
+        { divider: true},
+      ],
+      item2: [
+         {
+          avatar: require('./assets/UserTwitter.png'),
+          title: 'Username',
+          subtitle: "@User_Login",
+        },
+      ],
+      Dialog: [
+        { title: "เพิ่มบัญชีที่มีอยู่แล้ว" },
+        { title: "ออกจากระบบ" ,check: true},
+      ],
+      Report: [
+        { icon: "mdi-emoticon-sad-outline" ,reports: "ความนิยมนี้เป็นแสปม"},
+        { icon: "mdi-emoticon-sad-outline" ,reports: "ความนิยมนี้ไม่เหมาะสมหรือเป็นอันตราย"},
+        { icon: "mdi-emoticon-sad-outline" ,reports: "ความนิยมนี้มีความเหมือนกัน"},
+        { icon: "mdi-emoticon-sad-outline" ,reports: "ความนิยมนี้มีคุณภาพต่ำ"},
+      ],
+      windowWidth: window.innerWidth,
+    //New Data
+    //
+  }),
+
+  mounted() {
+    window.addEventListener("resize", () => {
+      this.windowWidth = window.innerWidth;
+    });
+  },
+  computed: {
+    isMobile() {
+      return this.windowWidth <= 800;
+    },
+  },
+};
 </script>
 
+<style scoped>
+  .hove:hover{
+    color: #42A5F5
+  }
+</style>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.scroll .v-navigation-drawer__content::-webkit-scrollbar {
+  display: none;
+}
+.borderd .v-toolbar__content{
+  border-bottom: 1px solid #E0E0E0;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,338 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+  <v-container style="padding:0px">
+    <!-- Post -->
+  <v-form
+    ref="form"
+    v-model="valid"
+  >
+  <v-list dense style="padding:0px;">
+    <v-list-item two-line>
+
+      <v-list-item-avatar dense style="padding:0px ;margin-top:1px">
+       <v-img
+          :src="require('../assets/UserTwitter.png')"
+       />
+      </v-list-item-avatar>
+
+    <v-list-item-content dense style="padding:0px; margin:0px">
+    <v-text-field
+    class="title font-weight-thin "
+      light
+      solo
+      flat
+      dense
+      :rules="nameRules"
+      placeholder="มีอะไรเกิดขึ้นบ้าง"
+    ></v-text-field>
+    </v-list-item-content>
+  </v-list-item>
+
+
+
+  <v-list-item dense>
+     <v-layout-row class="ml-12">
+     <v-list-item-action
+        style="margin-right:0px ;margin-left:0px"
+        v-for="item in icons"
+        :key="item.text"
+      >
+
+      <v-btn icon fab dark small color="#42A5F5" >
+      <v-icon >{{item.icon}}</v-icon>
+      </v-btn>
+
+    </v-list-item-action>
+
+    </v-layout-row>
+    <v-spacer></v-spacer>
+    <v-btn dark color="#42A5F5" class="mr-5" rounded >
+        ทวีต
+    </v-btn>
+  </v-list-item>
+  </v-list>
+
+  <v-divider></v-divider>
+
+  <div style="height:10px; background-color:#f5f8fa">
+
   </div>
+
+  </v-form>
+  <v-divider></v-divider>
+
+    <!-- Pose -->
+
+    <!-- Status -->
+  <v-form
+    ref="form"
+    v-model="valid"
+
+  >
+  <v-list v-for="(list,index) in content" :key="index" style="padding:0px">
+    <div  @click="doSomething()">
+    <v-list-item v-if="index != 4">
+
+      <v-list-item-avatar style="padding:0px ">
+       <v-img
+          :src="list.avatar"
+       />
+      </v-list-item-avatar>
+
+    <v-list-item-content three-line>
+
+            <v-list-item-subtitle class="caption font-weight-thin">
+              <v-icon v-if="list.tweetby != ''" small>mdi-twitter-retweet</v-icon>
+              {{list.tweetby}}
+              </v-list-item-subtitle>
+
+
+
+            <v-list-item-title class="subtitle font-weight-bold">{{list.Account}}
+
+            <v-text class="ml-2 subtitle font-weight-light">{{list.link}}</v-text>
+              •
+             <v-text class=" subtitle font-weight-light">{{list.time}}</v-text>
+
+                <v-btn icon rounded class="font-weight-bold " light >
+                  <v-icon>mdi-chevron-down</v-icon>
+                  </v-btn>
+
+              </v-list-item-title>
+
+
+            <v-list-item-title class="text-wrap">
+               {{list.text}}
+
+            </v-list-item-title>
+
+
+
+    </v-list-item-content>
+
+  </v-list-item>
+    </div>
+
+  <v-list-item v-if="list.image != '' && index != 4" class="ml-12 mr-5">
+    <v-card  flat>
+    <v-img :src="list.image"/>
+    </v-card>
+  </v-list-item>
+
+
+  <v-list-item v-if="index != 4" class="ml-10" >
+    <v-col v-for="list in posticon"
+        :key="list.text">
+     <v-list-item-action>
+       <v-row >
+         <div>
+          <v-btn rounded text :class="list.text">
+        <v-btn
+                icon
+                light small
+                >
+          <v-icon>{{list.icon}}</v-icon>
+        </v-btn>
+
+        <v-text class="caption">{{list.tweet}}</v-text>
+           </v-btn>
+        </div>
+
+       </v-row>
+    </v-list-item-action>
+    </v-col>
+  </v-list-item>
+  <v-divider></v-divider>
+
+<!-- ติดตาม -->
+
+
+  <v-list v-if="list.follow == true" style="padding-top:0 ;padding-bottom:0" >
+
+    <div style="height:10px; background-color:#f5f8fa">
+
+  </div>
+
+  <v-list two-line>
+     <v-toolbar-title class="ml-5 my-2 title font-weight-bold">ติดตามใครดี
+        <v-btn icon>
+        <v-icon>mdi-chevron-down</v-icon>
+      </v-btn>
+     </v-toolbar-title>
+
+
+
+      <v-divider></v-divider>
+
+      <template v-for="(item, index) in items" >
+
+        <v-divider
+          v-if="item.divider"
+          :key="index"
+        ></v-divider>
+
+        <v-list-item
+          v-else
+          :key="item.title"
+          @click="doSomething()"
+        >
+          <v-list-item-avatar>
+            <v-img :src="item.avatar"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title class="font-weight-bold" v-html="item.title"></v-list-item-title>
+            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+          <v-spacer></v-spacer>
+          </v-list-item-content>
+
+          <v-btn rounded text outlined class="font-weight-bold" color="#42A5F5" style="border: 1px solid currentColor;"> ติดตาม</v-btn>
+        </v-list-item>
+
+      </template>
+    </v-list>
+
+    <v-btn
+      text
+      rounded
+      block
+      light
+      color="#42A5F5"
+      class="font-weight-bold"
+      >
+        แสดงมากขึ้น
+      </v-btn>
+      <v-divider></v-divider>
+      <div style="height:10px; background-color:#f5f8fa">
+
+  </div>
+
+  </v-list>
+
+<!-- ติดตาม -->
+
+
+  </v-list>
+
+  </v-form>
+
+    <!-- Status -->
+  </v-container>
 </template>
 
+
 <script>
-export default {
+  export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    data: () => ({
+      icons: [
+        {icon: "mdi-image", text: "Image"},
+        {icon: "mdi-gif", text: "GIF"},
+        {icon: "mdi-poll-box-outline", text: "Poll"},
+        {icon: "mdi-emoticon-happy-outline", text: "Happy"},
+        {icon: "mdi-calendar-refresh-outline", text: "Calender"},
+      ],
+      content: [
+        {
+        avatar: require('../assets/Lisa.jpg'),
+        tweetby: "ไอคอนกำลังเข้าเมือง ได้รีทวีต",
+        Account:"BLACKPINK LISA 리사",
+        link: "@lalaIalisaa_m",
+        time:'1 ชม.',
+        text:"Guys don't forget to follow blackpink and also the members on Mubeat app, and collect heart beats! Download now guys..",
+        image: require('../assets/lisapose.jpg'),
+        },
+
+        {
+        avatar: require('../assets/HRK.jpg'),
+        tweetby: "ไอคอนกำลังเข้าเมือง ได้รีทวีต",
+        Account:"พี่เป็นนักยูทูป",
+        link: "@MisterHeartz",
+        time:'2 ชม.',
+        text:"เอางี้ก่อนนะ ผมไม่ได้ว่าคนที่เรียกร้องให้ผมออกมาแสดงอะไรบางอย่างนะ เพราะผมเข้าใจเจตนาจริง ๆ อันนี้อย่าเข้าใจผมผิดน้า ผมแค่ว่าคนที่ด่าเปิดหัวผมก่อนเลยเฉย ๆ แบบน้อยใจอะไรงี้ เอาแบบนี้มั้ย ผมเปิดโอกาสให้ถามใต้ทวีตนี้ดีกว่าผมจะลองตอบดู  อยากปรับความเข้าใจกันมากเลย TT",
+        image:"",
+        },
+        {
+        avatar: require('../assets/Artgerm.jpg'),
+        tweetby: "",
+        Account:"Stanley Artgerm Lau",
+        link: "@Stanley Artgerm Lau",
+        time:'5 ชม.',
+        text:"Alright. Took a moment to add some colors to this. :) #sailormoon #sailormoonredraw",
+        image: require('../assets/Artgermpose.jpg'),
+        },
+        {
+        avatar: require('../assets/six.jpg'),
+        tweetby: "ไอคอนกำลังเข้าเมือง ได้รีทวีต",
+        Account:"SIXMOREVODKA",
+        link: "@sixmorevodka",
+        time:'5 ชม.',
+        text:"We published a truckload of art from Rising Tides on our website, as well as concepts and sketches for the new cards.",
+        image: require('../assets/monika.jpg'),
+        },
+        {follow: true},
+         {
+        avatar: require('../assets/ekko.jpg'),
+        tweetby: "",
+        Account:"League of Legends",
+        link: "@LeagueOfLegends",
+        time:'10 ชม.',
+        text:"Patch 10.11 Highlights",
+        image: require('../assets/lolastro.jpg'),
+        },
+         {
+        avatar: require('../assets/porter.jpg'),
+        tweetby: "ไอคอนกำลังเข้าเมือง ได้รีทวีต",
+        Account:"porter robinson",
+        link: "@porterrobinson",
+        time:'20 ชม.',
+        text:"GET YOUR WISH (OFFICIAL MUSIC VIDEO)",
+        image: require('../assets/getyourwish.jpg'),
+        },
+      ],
+      posticon: [
+        {text:"comment" ,styles:"comment", icon:"mdi-comment-outline", tweet:"59K"},
+        {text:"retweet" ,styles:"retweet", icon:"mdi-twitter-retweet", tweet:"61K"},
+        {text:"heart" ,styles:"heart", icon:"mdi-heart-outline", tweet:"70K"},
+        {text:"shared" ,styles:"shared", icon:"mdi-export-variant"},
+      ],
+      items: [
+        {
+          avatar: require('../assets/ilya.jpg'),
+          title: 'イリヤ・クブシノブ',
+          subtitle: "@Kuvshinov_Ilya",
+        },
+        { divider: true},
+        {
+          avatar: require('../assets/vanoss.jpg'),
+          title: 'VanossGaming',
+          subtitle: "@VanossGaming",
+        },
+        { divider: true},
+        {
+          avatar: require('../assets/ape.jpg'),
+          title: 'ConcernedApe',
+          subtitle: "@ConcernedApe",
+        },
+        { divider: true},
+      ],
+
+   }),
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .comment:hover{
+    color: #42A5F5
+  }
+  .retweet:hover{
+    color: #4CAF50
+  }
+  .heart:hover{
+    color: #F06292
+  }
+  .shared:hover{
+    color: #42A5F5
+  }
 </style>
+
